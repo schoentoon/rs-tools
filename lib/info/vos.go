9065e01f805c9f80ac55087cf497aa103fb73c5b
Toon Schoenmakers <nighteyes1993@gmail.com>
2020-11-26 16:46:49 +0100

    Added a command to figure out which could be the next voice of seren
    According to the wiki https://runescape.wiki/w/Voice_of_Seren#Randomness_of_the_voice
    it will never be the current voice of seren, or the previous one. Leaving us with 4 options.
    So basically we look at the history and just return all clans but the current voice of seren
    and the previous one.

--- lib/info/vos.go
+++ lib/info/vos.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 )
 
 type ElvenClan int8
@@ -22,6 +23,16 @@ const (
 	Meilyr
 	Trahaearn
 )
+
+func parseElvenClan(in string) (ElvenClan, error) {
+	for clan := Amlodd; clan <= Trahaearn; clan++ {
+		if clan.String() == in {
+			return clan, nil
+		}
+	}
+
+	return Invalid, fmt.Errorf("Invalid elven clan: %s", in)
+}
 
 func VoiceOfSeren(client *http.Client) ([]ElvenClan, error) {
 	params := url.Values{
@@ -56,19 +67,61 @@ func VoiceOfSeren(client *http.Client) ([]ElvenClan, error) {
 	split := strings.Split(wrapper.Query.AllMessages[0].Data, ",")
 
 	out := make([]ElvenClan, len(split)) // should always be 2 lol
-	for clan := ElvenClan(0); clan <= Trahaearn; clan++ {
-		for i, current := range split {
-			if clan.String() == current {
-				out[i] = clan
-			}
+	for i := 0; i < 2; i++ {
+		clan, err := parseElvenClan(split[i])
+		if err != nil {
+			return nil, err
 		}
-	}
-
-	for _, clan := range out {
-		if clan == Invalid {
-			return nil, fmt.Errorf("Something went wrong parsing the VoS: %#+v", wrapper)
-		}
+		out[i] = clan
 	}
 
 	return out, nil
+}
+
+func PossibleNextVoiceOfSeren(client *http.Client) ([]ElvenClan, error) {
+	resp, err := client.Get("https://chisel.weirdgloop.org/api/runescape/vos/history")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	wrapper := struct {
+		Data []struct {
+			Districts []string  `json:"districts"`
+			Date      time.Time `json:"date"`
+		} `json:"data"`
+	}{}
+	err = json.NewDecoder(resp.Body).Decode(&wrapper)
+	if err != nil {
+		return nil, err
+	}
+
+	previouslyClans := make([]ElvenClan, 0, 4)
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			clan, err := parseElvenClan(wrapper.Data[i].Districts[j])
+			if err != nil {
+				return nil, err
+			}
+			previouslyClans = append(previouslyClans, clan)
+		}
+	}
+
+	previously := func(e ElvenClan) bool {
+		for _, clan := range previouslyClans {
+			if e == clan {
+				return true
+			}
+		}
+		return false
+	}
+
+	inverted := make([]ElvenClan, 0, 4)
+	for clan := Amlodd; clan <= Trahaearn; clan++ {
+		if !previously(clan) {
+			inverted = append(inverted, clan)
+		}
+	}
+
+	return inverted, nil
 }
--- lib/info/vos_test.go
+++ lib/info/vos_test.go
@@ -9,6 +9,26 @@ import (
 	"gitlab.com/schoentoon/rs-tools/lib"
 )
 
+func TestParseElvenClan(t *testing.T) {
+	cases := []struct {
+		in           string
+		expectedClan ElvenClan
+		expectError  bool
+	}{
+		{"Amlodd", Amlodd, false},
+		{"Not a clan", Invalid, true},
+	}
+
+	for _, c := range cases {
+		clan, err := parseElvenClan(c.in)
+		if c.expectError {
+			assert.Error(t, err)
+		} else {
+			assert.Equal(t, c.expectedClan, clan)
+		}
+	}
+}
+
 func TestVoiceOfSeren(t *testing.T) {
 	client := lib.NewTestClient(func(req *http.Request) (int, string) {
 		data, err := ioutil.ReadFile("testdata/vos.json")
@@ -33,4 +53,24 @@ func TestVoiceOfSerenEmptyJSON(t *testing.T) {
 	res, err := VoiceOfSeren(client)
 	assert.Error(t, err)
 	assert.Nil(t, res)
+}
+
+func TestPossibleNextVoiceOfSeren(t *testing.T) {
+	client := lib.NewTestClient(func(req *http.Request) (int, string) {
+		data, err := ioutil.ReadFile("testdata/vos_history.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		return 200, string(data)
+	})
+
+	res, err := PossibleNextVoiceOfSeren(client)
+	assert.Nil(t, err)
+
+	assert.Len(t, res, 4)
+
+	assert.Contains(t, res, Amlodd)
+	assert.Contains(t, res, Cadarn)
+	assert.Contains(t, res, Meilyr)
+	assert.Contains(t, res, Trahaearn)
 }
